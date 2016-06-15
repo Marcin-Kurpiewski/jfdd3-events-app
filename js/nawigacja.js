6cbbec4ffb252319a719ecdff9e2fc65c073121d
ElwiraKobusiewicz <elwirak4@gmail.com>
2016-06-15 16:58:16 +0200

    szkic kalendarza2

--- js/nawigacja.js
+++ js/nawigacja.js
@@ -2,11 +2,14 @@
  * Created by Elwira on 2016-06-14.
  */
 $(document).ready(function () {
-    $( ".guzik-lewy" ).click(function() {
-        $( ".nawigacja-lista" ).animate({ "left": "+=50px" }, "slow" );
+    $( "#guzik-lewy" ).on('click', function() {
+        $( ".nawigacja-lista" ).animate({ backgroundPositionX:0}, 500, function(){
+            $(this).stop();
+        })
+            .animate({ "left": "-=50px" }, "slow" );
     });
 
     $( "#guzik-prawy" ).click(function(){
-        $( ".nawigacja-lista" ).animate({ "left": "-=50px" }, "slow" );
+        $( ".nawigacja-lista" ).animate({ "right": "-=50px" }, "slow" );
     });
 })
