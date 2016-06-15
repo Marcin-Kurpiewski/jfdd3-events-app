035686fcf9b2ecddb9d6bcf04397287fb57f5d78
Elwirka <elwirak@interia.pl>
2016-06-16 01:07:21 +0200

    zmiana w nawigacji

--- js/nawigacja.js
+++ js/nawigacja.js
@@ -1,23 +1,12 @@
 /**
  * Created by Elwira on 2016-06-14.
  */
-/*$(document).ready(function () {
-    $( ".lewy" ).on('click', function() {
-        $( ".nawigacja-lista" ).animate({ backgroundPositionX:0}, 500, function(){
-            $(this).stop();
-        })
-            .animate({ "left": "-=50px" }, "slow" );
-    });
 
-    $( ".prawy" ).click(function(){
-        $( ".nawigacja-lista" ).animate({ "right": "-=50px" }, "slow" );
-    });
-})*/
 
-$( "#prawy" ).click(function() {
-    $( ".nawigacja-lista" ).animate({ "right": "+=100px" }, "slow" );
+$( "#right" ).click(function() {
+    $( ".block" ).animate({ "left": "+=150px" }, "slow" );
 });
 
-$( "#lewy" ).click(function(){
-    $( ".nawigacja-lista" ).animate({ "left": "-=100px" }, "slow" );
+$( "#left" ).click(function(){
+    $( ".block" ).animate({ "left": "-=150px" }, "slow" );
 });
