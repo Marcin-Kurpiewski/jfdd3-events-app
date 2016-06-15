809e92719b470fb5b927ad4d6c6f91dbadccf059
Elwirka <elwirak@interia.pl>
2016-06-15 22:51:15 +0200

    opracowanie css i dodanie skryptu js1

--- js/nawigacja.js
+++ js/nawigacja.js
@@ -1,15 +1,23 @@
 /**
  * Created by Elwira on 2016-06-14.
  */
-$(document).ready(function () {
-    $( "#guzik-lewy" ).on('click', function() {
+/*$(document).ready(function () {
+    $( ".lewy" ).on('click', function() {
         $( ".nawigacja-lista" ).animate({ backgroundPositionX:0}, 500, function(){
             $(this).stop();
         })
             .animate({ "left": "-=50px" }, "slow" );
     });
 
-    $( "#guzik-prawy" ).click(function(){
+    $( ".prawy" ).click(function(){
         $( ".nawigacja-lista" ).animate({ "right": "-=50px" }, "slow" );
     });
-})
+})*/
+
+$( "#prawy" ).click(function() {
+    $( ".lista" ).animate({ "left": "+=150px" }, "slow" );
+});
+
+$( "#lewy" ).click(function(){
+    $( ".lista" ).animate({ "left": "-=150px" }, "slow" );
+});
