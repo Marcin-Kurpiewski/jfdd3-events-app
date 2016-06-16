93a714d78b5371205e5005da581501c0795ee87e
ElwiraKobusiewicz <elwirak4@gmail.com>
2016-06-16 16:38:06 +0200

    szkic kalendarza z nową funkcją animacji menu

--- js/nawigacja.js
+++ js/nawigacja.js
@@ -2,11 +2,26 @@
  * Created by Elwira on 2016-06-14.
  */
 
+var position = 0;
+//for (var position = 0; position < 4; position ++) {
+    $("#right").click(function () {
+        var $block = $(".block"), $left = $("#left");
+        if( $block.position().left - $left.position().left >= -30 ){
+            $block.css('left', '-30px');
+        }else{
+            $block.animate({"left": "+=150px"}, "slow");
+        }
+    })
+//};
 
-$( "#right" ).click(function() {
-    $( ".block" ).animate({ "left": "+=150px" }, "slow" );
-});
 
-$( "#left" ).click(function(){
-    $( ".block" ).animate({ "left": "-=150px" }, "slow" );
-});
+    $("#left").click(function () {
+        var $block = $(".block"), $right = $("#right"), rightPoint = $right.position().left - $block.width();
+        console.log( rightPoint );
+        console.log( $block.position().left );
+        if( $block.position().left < rightPoint ){
+            $block.css('left', rightPoint + 'px');
+        }else{
+            $block.animate({"left": "-=150px"}, "slow");
+        }
+    });
